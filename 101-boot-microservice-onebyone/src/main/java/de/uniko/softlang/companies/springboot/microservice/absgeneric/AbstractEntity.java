6d24f6706eb9b64dcf1d64f0af88350baabca684
derzufall <github@bounce-soft.de>
2015-11-03 19:34:23 +0100

    intoduced basic project setup - step 1

---
+++ 101-boot-microservice-onebyone/src/main/java/de/uniko/softlang/companies/springboot/microservice/absgeneric/AbstractEntity.java
@@ -0,0 +1,30 @@
+/*
+ * license header
+ */
+package de.uniko.softlang.companies.springboot.microservice.absgeneric;
+
+import java.io.Serializable;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+/**
+ *
+ * @author derzufall
+ */
+@MappedSuperclass
+public class AbstractEntity implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }    
+}
